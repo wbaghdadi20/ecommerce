9ee03112927cb56844cc82f22b7884ba2ce3f589
wbaghdadi20 <wbaghdadi5@gmail.com>
2024-07-11 22:47:06 -0700

    done

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const functions = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51PbVtcRtEGRMpbLDAiBUPHdnPoZUPju4VsxU5GcZ2VHkpE46dKWDL4b29TV6sCbVMLixo9DtuximvnJrldvYlmSZ00DetzD1hl"
+    "sk_test_51PbVtcRtEGRMpbLDAiBUPHdnPoZUPju4VsxU5GcZ2VHkpE4" +
+    "6dKWDL4b29TV6sCbVMLixo9DtuximvnJrldvYlmSZ00DetzD1hl",
 );
 
 // API
@@ -13,22 +12,22 @@ const stripe = require("stripe")(
 const app = express();
 
 // - Middlewares
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
 
-// - API rotues
+// - API routes
 app.get("/", (req, res) => res.status(200).send("Hello World"));
 
 app.post("/payment/create", async (req, res) => {
   const total = req.query.total;
-  console.log("Payment request recieved for this amount >>>", total);
+  console.log("Payment request received for this amount >>>", total);
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total,
     currency: "usd",
   });
 
-  //   OK - Created
+  // OK - Created
   res.status(201).send({
     clientSecret: paymentIntent.client_secret,
   });
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-c002b/us-central1/api", // The API (cloud function) URL
+  baseURL:
+    // "http://127.0.0.1:5001/clone-c002b/us-central1/api", // local host
+    "https://api-nbabtgg6ba-uc.a.run.app",
 });
 
 export default instance;
